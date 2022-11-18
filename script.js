dbc618b774c05a3ef0e3504ad26dcf2fcfdb6a0c
Kyo young Lim <limkyouyou@gmail.com>
2022-11-18 10:32:05 +0900

    Write basic algorithm for the game

---
+++ script.js
@@ -0,0 +1,23 @@
+// game start, prompt message 'Do you want to play rock, paper, scissors with the machine?'.
+
+// the user click cancel, game closes
+
+// the user click okay
+// prompt user for input, 'Type-in rock, paper, or scissors then click okay.'
+
+// the user input and click okay
+// computer randomly pick one out of three options
+
+// compare the choice between the user and the computer, give a point to the winner
+
+// count round played
+
+// count round won for each player
+
+// prompt message result of the round, rounds played, and each player's score, the user press okay to move on to the next round
+
+// when a player wins three rounds first or at the end of five round, game ends
+
+// when game ends, prompt message 'The winner of the game is you/the machine!', 'Score is You: #, The machiine: #', 'Do you want to play again?' 
+
+// the user press okay to play again
