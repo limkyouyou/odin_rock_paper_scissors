3add01fc179b15e1d489c277616ffd94060d59cd
Kyo young Lim <limkyouyou@gmail.com>
2022-11-22 10:42:00 +0900

    Create a new JS as intended from the assignment

---
+++ intended.js
@@ -0,0 +1,65 @@
+
+// Create roundCount variable and assign it to 0
+let roundCount = 0;
+// Create playerScore variable and assign it to 0
+let playerScore = 0;
+// Create computerScore variable and assign it to 0
+let computerScore = 0;
+// Create playerHand variable
+let playerChoice;
+
+// Create random computer choice function
+function getComputerChoice() {
+  let random = Math.floor(Math.random()*3);
+  if (random === 0) {
+    return "rock";
+  } else if (random === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+// Create playRound function with two parameters, make playerSelection case-insensitive by transforming all its input to lower case, add player score and computer score
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore += 1;
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+    computerScore += 1;
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+    computerScore += 1;
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+    playerScore += 1;
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+    playerScore += 1;
+    return "You Win! Scissors beats Paper";
+  } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+    playerScore += 1;
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection.toLowerCase() === computerSelection) {
+    return "You draw this round!"
+  }
+}
+
+// Create game function, run playRound function for 5 times, keep score and report a winner or loser at the end
+function game() {
+  for (let i = 0; i < 5; i++) { // loop for 5 rounds
+    roundCount += 1; // add one to round every loop
+    playerChoice = prompt("This is the intended version. Type-in rock, paper, or scissors") // prompt player to choose a hand
+    console.log(playRound(playerChoice, getComputerChoice())); // run playRound function with playerChoice and getComputerChoice function as a parameter then console out
+
+  }
+  if (computerScore > playerScore) { // return winner or loser or draw depending on the score
+    return `You are the Loser! Your score is ${playerScore} and The computer's score is ${computerScore}`;
+  } else if (playerScore > computerScore) {
+    return `You are the Winner! Your score is ${playerScore} and The computer's score is ${computerScore}`;
+  } else if (playerScore === computerScore) {
+    return `You draw this round! Your score is ${playerScore} and The computer's score is ${computerScore}`;
+  }
+}
+
+console.log(game()); // execute game function and log out its return
